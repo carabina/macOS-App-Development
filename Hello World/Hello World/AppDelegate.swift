2ea41693e86acdd0deb7a68786840caef36c0ddc
vinoth <vinothvino42@gmail.com>
2018-06-19 10:02:02 +0530

    Add Hello World

---
+++ Hello World/Hello World/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hello World
+//
+//  Created by Vinoth Vino on 19/06/18.
+//  Copyright © 2018 Vinoth Vino. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Vinoth Vino on 19/06/18.
+//  Copyright © 2018 Vinoth Vino. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
